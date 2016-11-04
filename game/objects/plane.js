f899e38db056cb84f58a46b1496387fb505ff68e
gryp17 <plamen.ivanov17@gmail.com>
2016-11-04 15:53:42 +0200

    Added the plane crashing when it reaches the ground

--- game/objects/plane.js
+++ game/objects/plane.js
@@ -3,9 +3,12 @@ function Plane(planeContext, planeType) {
 	this.context = planeContext.context;
 	this.canvas = planeContext.canvas;
 	this.planeStats = PLANES_STATS[planeType];
+	this.currentHealth = this.planeStats.HEALTH;
 	this.planeImages = IMAGE_REPOSITORY.images.PLANES[planeType];
 	this.spriteIndex = 0;
 	this.currentImage = this.planeImages.SPRITE[0];
+	this.disabled = false; //the plane is disabled and can't be controlled anymore
+	this.crashed = false; //the plane has crashed to the ground
 	this.dx = 1;
 	this.x = 300;
 	this.dy = 0;
@@ -18,20 +21,34 @@ function Plane(planeContext, planeType) {
 	 * Draws the plane object
 	 */
 	this.draw = function () {
-		this.frames++;
 
-		//if the limit has been reached show the next sprite image
-		if (this.frames > this.limit) {
-			this.spriteIndex++;
+		//if the plane has crashed show the crashed image
+		if (this.crashed === true) {
+			this.currentImage = this.planeImages.CRASHED;
+		} 
+		//otherwise loop thru the sprite images
+		else {
+			this.frames++;
 
-			if (_.isUndefined(this.planeImages.SPRITE[this.spriteIndex])) {
-				this.spriteIndex = 0;
-				;
+			//if the limit has been reached show the next sprite image
+			if (this.frames > this.limit) {
+				this.spriteIndex++;
+
+				if (_.isUndefined(this.planeImages.SPRITE[this.spriteIndex])) {
+					this.spriteIndex = 0;
+					;
+				}
+
+				this.currentImage = this.planeImages.SPRITE[this.spriteIndex];
+
+				this.frames = 0;
 			}
+		}
 
-			this.currentImage = this.planeImages.SPRITE[this.spriteIndex];
-
-			this.frames = 0;
+		//if the plane is too damaged disable it and crash it (only do this if it hasn't crashed yet)
+		if(this.currentHealth <= 0 && this.crashed === false){
+			this.disabled = true;
+			this.dy = 2;
 		}
 
 		//clear the rectangle around the plane
@@ -42,40 +59,40 @@ function Plane(planeContext, planeType) {
 		this.y = this.y + this.dy;
 
 		this.checkForCollisions();
-		
+				
 		//if the plane is ascending or descending rotate it
-		if(this.dy > 0){
+		if (this.dy > 0) {
 			this.rotatePlane(this.dy * this.planeStats.DESCEND_SPEED);
-		} else if(this.dy < 0){
+		} else if (this.dy < 0) {
 			this.rotatePlane(this.dy * this.planeStats.CLIMB_SPEED);
 		}
 		//otherwise draw it in it's normal state
-		else{
+		else {
 			this.context.drawImage(this.currentImage, this.x, this.y);
 		}
-		
+
 	};
-	
+
 	/**
 	 * Rotates the plane
 	 * @param {int} angle
 	 */
-	this.rotatePlane = function (angle){
-		this.context.save(); 
+	this.rotatePlane = function (angle) {
+		this.context.save();
 
 		//move to the middle of where we want to draw our image
-		this.context.translate(this.x + this.currentImage.width/2, this.y + this.currentImage.height/2);
+		this.context.translate(this.x + this.currentImage.width / 2, this.y + this.currentImage.height / 2);
 
 		//rotate around that point, converting our angle from degrees to radians 
-		this.context.rotate(angle * Math.PI/180);
+		this.context.rotate(angle * Math.PI / 180);
 
 		//draw it up and to the left by half the width and height of the image 
-		this.context.drawImage(this.currentImage, -(this.currentImage.width/2), -(this.currentImage.height/2));
+		this.context.drawImage(this.currentImage, -(this.currentImage.width / 2), -(this.currentImage.height / 2));
 
 		//and restore the co-ords to how they were when we began
-		this.context.restore(); 
+		this.context.restore();
 	};
-	
+
 
 	/**
 	 * Checks if the plane has reached the top, bottom, left or right end of the screen
@@ -88,7 +105,7 @@ function Plane(planeContext, planeType) {
 		}
 
 		//left end of screen
-		if (this.x < 0) {
+		if (this.x < 0 && this.crashed === false) {
 			this.x = 0;
 			this.dx = 0;
 		}
@@ -103,38 +120,46 @@ function Plane(planeContext, planeType) {
 		if (this.y + this.currentImage.height > this.canvas.height - 50) {
 			this.y = this.canvas.height - this.currentImage.height - 50;
 			this.dy = 0;
+			this.dx = -2;
+			this.disabled = true;
+			this.crashed = true;
 		}
 	};
 
 	//keyboard controls
 	$("body").keydown(function (e) {
 
-		//left
-		if (e.which === 37 || e.which === 65) {
-			if (self.dx > -1) {
-				self.dx = self.dx - 1;
-			}
-		}
+		//respond to the controls only if the plane is not disabled
+		if (self.disabled === false) {
 
-		//right
-		if (e.which === 39 || e.which === 68) {
-			if (self.dx < self.planeStats.MAX_SPEED) {
-				self.dx = self.dx + 1;
+			//left
+			if (e.which === 37 || e.which === 65) {
+				if (self.dx > -1) {
+					self.dx = self.dx - 1;
+				}
 			}
-		}
 
-		//top
-		if (e.which === 38 || e.which === 87) {
-			if (self.dy > -3) {
-				self.dy = self.dy - 1;
+			//right
+			if (e.which === 39 || e.which === 68) {
+				if (self.dx < self.planeStats.MAX_SPEED) {
+					self.dx = self.dx + 1;
+				}
 			}
-		}
 
-		//bottom
-		if (e.which === 40 || e.which === 83) {
-			if (self.dy < 3) {
-				self.dy = self.dy + 1;
+			//top
+			if (e.which === 38 || e.which === 87) {
+				if (self.dy > -3) {
+					self.dy = self.dy - 1;
+				}
 			}
+
+			//bottom
+			if (e.which === 40 || e.which === 83) {
+				if (self.dy < 3) {
+					self.dy = self.dy + 1;
+				}
+			}
+
 		}
 
 	});
