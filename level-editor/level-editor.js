77e55d705e9045e5003e46fc3a69e4cc2b753d09
plamen <plamen.ivanov17@gmail.com>
2018-05-19 18:08:50 +0300

    Added the game objects drag and drop functionality

--- level-editor/level-editor.js
+++ level-editor/level-editor.js
@@ -1,10 +1,15 @@
 function LevelEditor(container) {
 	var self = this;
 	
+	//selectors
 	this.container = container;
 	this.backgroundContainer = this.container.find(".background-container");
 	this.controlsContainer = this.container.find(".controls-container");
 	this.controls = this.container.find(".controls");
+	
+	//mouse status
+	this.dragging = false;
+	this.draggedObject;
 
 	/**
 	 * Initialize the level editor by setting all event listeners
@@ -27,7 +32,88 @@ function LevelEditor(container) {
 		$(".theme").change(function () {
 			self.setLevelTheme($(this).val());
 		});
+		
+		//mousedown handler only for the game objects inside the controls
+		this.controls.find(".game-object").mousedown(function (e){
+			
+			//copy th egame object and move it to the mouse cursor position
+			self.draggedObject = $(this).clone();
+			self.draggedObject.css({position: "absolute"});
+			self.draggedObject.css({
+				top: e.pageY,
+				left: e.pageX
+			});
+			
+			//append it to the body temporarily
+			$("body").append(self.draggedObject);
+			
+			self.dragging = true;
+			e.preventDefault();
+		});
 
+		//on mouse up drop the object at the cursor position
+		$("body").mouseup(function (e) {
+
+			if (self.dragging) {
+				
+				//if the object was dropped outside of the level - don't do anything
+				if(e.pageY > self.backgroundContainer.height()){
+					return;
+				}
+				
+				self.draggedObject.css({
+					top: e.pageY,
+					left: e.pageX
+				});
+
+				//move the object from the "body" to the background container when it needs to be dropped
+				self.draggedObject.appendTo(self.backgroundContainer);
+
+				self.draggedObject = null;
+				self.dragging = false;
+
+				console.log(e.pageX + " --- " + e.pageY);
+			}
+
+		});
+		
+		//on mouse move - move the dragged object to the mouse cursor position
+		this.container.mousemove(function (e){
+			if(self.dragging){
+				self.draggedObject.css({
+					top: e.pageY,
+					left: e.pageX
+				});				
+			}
+		});
+		
+		//on save button click get all objects and normalize their X and Y coordinates
+		$(".save-level-button").click(function (){
+			var enemies = [];
+			var objects = self.backgroundContainer.find(".game-object");
+			
+			objects.each(function (){
+				var enemy = $(this);
+				
+				var x = parseInt(enemy.css("left").replace("px", ""));
+				var y = parseInt(enemy.css("top").replace("px", ""));
+				
+				//x = x + (x * 24.56/100); 
+				x = x + (x * 30/100); 
+				
+				//TODO: adjust Y as well
+				
+				enemies.push({
+					objectType: enemy.attr("data-object"),
+					arguments: {
+						x: x,
+						y: y
+					}
+				});
+			});
+			
+			console.log(enemies);
+		});
 
 		//show the controls
 		this.showControlsContainer();
