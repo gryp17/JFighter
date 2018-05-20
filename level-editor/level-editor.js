94bd4685ed5a2e7ba0bada384316d84f6f456a8c
plamen <plamen.ivanov17@gmail.com>
2018-05-20 19:27:26 +0300

    All game objects can now be moved or removed after they have been placed. Did some code refactoring.

--- level-editor/level-editor.js
+++ level-editor/level-editor.js
@@ -33,115 +33,172 @@ function LevelEditor(container) {
 	this.init = function () {
 
 		//move the level background on mouse wheel scroll
-		this.backgroundContainer.on("mousewheel", function (e) {
-			var scrollLeft = $(document).scrollLeft();
-			$(document).scrollLeft(scrollLeft + e.deltaY * 40);
-			e.preventDefault();
-		});
+		this.backgroundContainer.on("mousewheel", self.scrollBackground);
 
 		//on theme value change - update the level theme
-		$(".theme").change(function () {
-			self.setLevelTheme($(this).val());
-		});
+		$(".theme").change(self.setLevelTheme);
 		
 		//mousedown handler only for the game objects inside the controls
-		this.controls.find(".game-object").mousedown(function (e){
-			
-			//copy th egame object and move it to the mouse cursor position
-			self.draggedObject = $(this).clone();
-			self.draggedObject.css({position: "absolute"});
-			self.draggedObject.css({
-				top: e.pageY,
-				left: e.pageX
-			});
-			
-			//append it to the body temporarily
-			$("body").append(self.draggedObject);
-			
-			self.dragging = true;
-			e.preventDefault();
-		});
-
+		this.controls.find(".game-object").mousedown(self.addGameObject);
+		
+		//mousedown handler only for the game objects inside the background container
+		$("body").on("mousedown", ".background-container .game-object", self.selectGameObject);
+		
 		//on mouse up drop the object at the cursor position
-		$("body").mouseup(function (e) {
-
-			if (self.dragging) {
-				
-				//if the object was dropped outside of the level - don't do anything
-				if(e.pageY > self.backgroundContainer.height()){
-					return;
-				}
-				
-				var top = e.pageY;
-				var left = e.pageX;
-				
-				//if the object is a ground object - anchor it to the ground by setting the correct "top" parameter
-				if(self.groundObjects[self.draggedObject.attr("data-object")]){
-					var groundData = self.groundObjects[self.draggedObject.attr("data-object")];
-					top = self.backgroundImageHeight - groundData.groundDistance - self.draggedObject.height();
-				}
-								
-				self.draggedObject.css({
-					top: top,
-					left: left
-				});
-
-				//move the object from the "body" to the background container when it needs to be dropped
-				self.draggedObject.appendTo(self.backgroundContainer);
-
-				self.draggedObject = null;
-				self.dragging = false;
-			}
-
-		});
+		$("body").mouseup(self.dropGameObject);
 		
 		//on mouse move - move the dragged object to the mouse cursor position
-		this.container.mousemove(function (e){
-			if(self.dragging){
-				self.draggedObject.css({
-					top: e.pageY,
-					left: e.pageX
-				});				
-			}
-		});
+		this.container.mousemove(self.dragGameObject);
 		
 		//on save button click get all objects and normalize their X and Y coordinates
-		$(".save-level-button").click(function (){
-			var enemies = [];
-			var objects = self.backgroundContainer.find(".game-object");
-			
-			objects.each(function (){
-				var enemy = $(this);
-				var offset = enemy.offset();
-				
-				var x = offset.left;
-				var y = offset.top;
-				
-				//normalize the Y coordinate
-				y = y - self.heightOffset;
-				
-				enemies.push({
-					objectType: enemy.attr("data-object"),
-					arguments: {
-						x: x,
-						y: y
-					}
-				});
-			});
-			
-			console.log(enemies);
-		});
+		$(".save-level-button").click(self.saveLevel);
 
 	};
 	
 	/**
-	 * Changed the selected theme/background
-	 * @param {String} theme
+	 * Changes the selected theme/background.
 	 */
-	this.setLevelTheme = function (theme){
+	this.setLevelTheme = function (){
+		var theme = $(this).val();
+		
 		var imagesPath = "img/levels/";
 		var extension = ".jpg";
-		this.backgroundContainer.css({backgroundImage: "url("+imagesPath+theme+extension+")"});
+		self.backgroundContainer.css({backgroundImage: "url("+imagesPath+theme+extension+")"});
+	};
+	
+	/**
+	 * Called when the mousewheel is scrolled. 
+	 * It makes the background scroll left or right.
+	 * @param {Object} e
+	 */
+	this.scrollBackground = function (e) {
+		var scrollLeft = $(document).scrollLeft();
+		$(document).scrollLeft(scrollLeft + e.deltaY * 40);
+		e.preventDefault();
+	};
+
+	/**
+	 * Called only when a game object is selected from the controls area
+	 * @param {Object} e
+	 */
+	this.addGameObject = function (e) {
+		//copy the egame object and move it to the mouse cursor position
+		self.draggedObject = $(this).clone();
+		self.draggedObject.css({position: "absolute"});
+		self.draggedObject.css({
+			top: e.pageY,
+			left: e.pageX
+		});
+
+		//append it to the body temporarily
+		$("body").append(self.draggedObject);
+
+		self.dragging = true;
+		e.preventDefault();
+	};
+	
+	/**
+	 * Called when a game object is selected inside the background container area
+	 * @param {Object} e
+	 */
+	this.selectGameObject = function (e) {
+		self.draggedObject = $(this);
+		self.dragging = true;
+		e.preventDefault();
+	};
+	
+	/**
+	 * Called when the mouseup event is fired anywhere inside the body
+	 * @param {Object} e
+	 */
+	this.dropGameObject = function (e) {
+		if (self.dragging) {
+
+			//if the object was dropped outside of the level - don't do anything
+			if (e.pageY > self.backgroundContainer.height()) {
+				return;
+			}
+
+			var top = e.pageY;
+			var left = e.pageX;
+
+			//if the object is a ground object - anchor it to the ground by setting the correct "top" parameter
+			if (self.groundObjects[self.draggedObject.attr("data-object")]) {
+				var groundData = self.groundObjects[self.draggedObject.attr("data-object")];
+				top = self.backgroundImageHeight - groundData.groundDistance - self.draggedObject.height();
+			}
+
+			self.draggedObject.css({
+				top: top,
+				left: left
+			});
+
+			//if the object doesn't have a remove button - add it
+			if (!self.draggedObject.has(".remove-btn").length) {
+
+				var removeBtn = $("<img>", {
+					class: "remove-btn",
+					src: "img/level-editor/icon-remove.png",
+					title: "Remove",
+					click: function (e) {
+						$(this).closest(".game-object").remove();
+						self.draggedObject = null;
+						self.dragging = false;
+						e.stopPropagation();
+					}
+				});
+
+				self.draggedObject.append(removeBtn);
+			}
+
+			//move the object from the "body" to the background container when it needs to be dropped
+			self.draggedObject.appendTo(self.backgroundContainer);
+
+			self.draggedObject = null;
+			self.dragging = false;
+		}
+	};
+	
+	/**
+	 * Called when the mouse move event is fired
+	 * @param {Object} e
+	 */
+	this.dragGameObject = function (e) {
+		if (self.dragging) {
+			self.draggedObject.css({
+				top: e.pageY,
+				left: e.pageX
+			});
+		}
+	};
+	
+	/**
+	 * Get all games objects from the DOM and normalize their coordinates
+	 */
+	this.saveLevel = function () {
+		var enemies = [];
+		var objects = self.backgroundContainer.find(".game-object");
+
+		objects.each(function () {
+			var enemy = $(this);
+			var offset = enemy.offset();
+
+			var x = offset.left;
+			var y = offset.top;
+
+			//normalize the Y coordinate
+			y = y - self.heightOffset;
+
+			enemies.push({
+				objectType: enemy.attr("data-object"),
+				arguments: {
+					x: x,
+					y: y
+				}
+			});
+		});
+
+		console.log(enemies);
 	};
 	
 }
