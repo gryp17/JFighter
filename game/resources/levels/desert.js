2bf35bef064187512fbfbe136aa537c1ef796bfd
plamen <plamen.ivanov17@gmail.com>
2018-06-03 16:59:49 +0300

    Added the default desert level

--- game/resources/levels/desert.js
+++ game/resources/levels/desert.js
@@ -4,48 +4,434 @@ var DESERT = {
 		TYPE: "normal"
 	},
 	GROUND_HEIGHT: 40,
-	CIVILIANS: [],
+	CIVILIANS: [
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 3448,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 3945,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 5118,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 6530,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 7468,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 7585,
+				y: 542
+			}
+		}
+	],
 	ENEMIES: [
 		{
-			objectType: "Bomber",
+			objectType: "Soldier",
 			arguments: {
-				x: 2700,
-				y: 200
+				x: 10412,
+				y: 541
 			}
 		},
 		{
-			objectType: "Bomber",
+			objectType: "Soldier",
 			arguments: {
-				x: 1700,
-				y: 300
+				x: 10378,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 10465,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 10876,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 11134,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 11163,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 11224,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 9877,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 9806,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 1677,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 1727,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 1756,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 1825,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2637,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2614,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2525,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2470,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4582,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4617,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4669,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8604,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8641,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8217,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8243,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8112,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2897,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2940,
+				y: 541
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 11267,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 11762,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 1415,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 2059,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 2664,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 8350,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 8870,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 9215,
+				y: 540
 			}
 		},
 		{
 			objectType: "Fighter",
 			arguments: {
-				x: 1200,
-				y: 400
+				x: 19176,
+				y: 168
 			}
 		},
 		{
 			objectType: "Fighter",
 			arguments: {
-				x: 1200,
-				y: 0
+				x: 17079,
+				y: -11
 			}
 		},
 		{
 			objectType: "Fighter",
 			arguments: {
-				x: 1400,
-				y: 100
+				x: 14560,
+				y: 171
 			}
 		},
 		{
 			objectType: "Fighter",
 			arguments: {
-				x: 1600,
-				y: 200
+				x: 15872,
+				y: 216
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 15392,
+				y: 71
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 14862,
+				y: -89
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 13667,
+				y: 257
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 13434,
+				y: 91
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 13062,
+				y: 230
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 12267,
+				y: 157
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 11663,
+				y: 66
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 8265,
+				y: 48
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 8791,
+				y: 189
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 9429,
+				y: 245
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 7168,
+				y: -115
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 5863,
+				y: -62
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 7652,
+				y: 129
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 4505,
+				y: 49
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 17719,
+				y: -123
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 17410,
+				y: -58
 			}
 		}
 	]
