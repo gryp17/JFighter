6a3360d88944252e2b5b61e1337e435cb88225fa
plamen <plamen.ivanov17@gmail.com>
2018-06-03 16:45:02 +0300

    Added the default winter level

--- game/resources/levels/winter.js
+++ game/resources/levels/winter.js
@@ -8,22 +8,127 @@ var WINTER = {
 		{
 			objectType: "Civilian",
 			arguments: {
-				x: 1050,
-				y: 550
+				x: 1437,
+				y: 542
 			}
 		},
 		{
 			objectType: "Civilian",
 			arguments: {
-				x: 1080,
-				y: 550
+				x: 1370,
+				y: 542
 			}
 		},
 		{
 			objectType: "Civilian",
 			arguments: {
-				x: 1110,
-				y: 550
+				x: 1267,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 1886,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 1838,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 1897,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 5027,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 5081,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 5108,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 5152,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 6195,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 6269,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 7666,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 7724,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 7832,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 9165,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 9097,
+				y: 542
+			}
+		},
+		{
+			objectType: "Civilian",
+			arguments: {
+				x: 9232,
+				y: 542
 			}
 		}
 	],
@@ -31,22 +136,351 @@ var WINTER = {
 		{
 			objectType: "Soldier",
 			arguments: {
-				x: 1200,
-				y: 550
+				x: 7241,
+				y: 541
 			}
 		},
 		{
 			objectType: "Soldier",
 			arguments: {
-				x: 1300,
-				y: 550
+				x: 7200,
+				y: 541
 			}
 		},
 		{
 			objectType: "Soldier",
 			arguments: {
-				x: 1350,
-				y: 550
+				x: 7163,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 5954,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 5901,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 5848,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2025,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2057,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2183,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2211,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2873,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2914,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 2941,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4280,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4388,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4340,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 4107,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8351,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8387,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8436,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8473,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8869,
+				y: 541
+			}
+		},
+		{
+			objectType: "Soldier",
+			arguments: {
+				x: 8681,
+				y: 541
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 1510,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 6927,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 2700,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 8680,
+				y: 540
+			}
+		},
+		{
+			objectType: "Sherman",
+			arguments: {
+				x: 8143,
+				y: 540
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 14783,
+				y: 133
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 15413,
+				y: 266
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 16016,
+				y: 99
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 16748,
+				y: 100
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 17300,
+				y: 248
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 18183,
+				y: 341
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 5204,
+				y: 131
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 3160,
+				y: 86
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 3780,
+				y: 389
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 8233,
+				y: 74
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 8718,
+				y: 202
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 9105,
+				y: 327
+			}
+		},
+		{
+			objectType: "Fighter",
+			arguments: {
+				x: 10448,
+				y: -3
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 12905,
+				y: -145
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 12362,
+				y: -98
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 11872,
+				y: 1
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 11237,
+				y: 92
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 7117,
+				y: 5
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 6376,
+				y: 84
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 5752,
+				y: 69
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 9786,
+				y: -1
+			}
+		},
+		{
+			objectType: "Bomber",
+			arguments: {
+				x: 10502,
+				y: 205
 			}
 		}
 	]
